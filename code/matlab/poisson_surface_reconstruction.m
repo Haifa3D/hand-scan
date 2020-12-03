d7332eacce7d20a113364f69788029458743fd68
Guy Yoffe <guy.yoffe@campus.technion.ac.il>
2020-12-03 10:56:38 +0200

    fixed poisson reconstruction string to char

--- code/matlab/poisson_surface_reconstruction.m
+++ code/matlab/poisson_surface_reconstruction.m
@@ -1,5 +1,10 @@
 function poisson_surface_reconstruction(inputfile, outputfile, depth, samplesPerNode)
 poisson_path = getenv('poisson_path');
+
+poisson_path = convertStringsToChars(poisson_path);
+inputfile = convertStringsToChars(inputfile);
+outputfile = convertStringsToChars(outputfile);
+
 command = ['"', poisson_path, '\PoissonRecon.exe" --in "', inputfile, '" --out "', outputfile,...
     '" --depth ', num2str(depth), ' --verbose --samplesPerNode ', num2str(samplesPerNode)];
 [ status, cmdout ] = system(command);
